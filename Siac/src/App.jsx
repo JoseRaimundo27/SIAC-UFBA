b5e6ec8baa8bd8216399671c7bf51dbe2eb47728
José <joseraimundoalves@hotmail.com>
2023-12-13 15:42:26 -0300

    Consumindo minha API

--- Siac/src/App.jsx
+++ Siac/src/App.jsx
@@ -1,12 +1,21 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import Login from "./Components/Login/login"
+import axios from 'axios'
 
 function App() {
-
+  const [dadosLogin, setDadosLogin] = useState({})
+  useEffect(()=>{
+    async function getDadosLogin(){
+      const res = await axios.get("http://localhost:3000/cadastro")
+      setDadosLogin(res.data)
+    } 
+    getDadosLogin();
+  },[])
 
   return (
-    <>
+    <><h1>{dadosLogin.email}</h1>
     <section>
       <Login/>
     </section>
