525a96dd043f323325804bae9f15e7fffa4c7089
José <joseraimundoalves@hotmail.com>
2023-12-21 15:54:59 -0300

    pagina not found

--- Siac/src/Pages/NoPage/nopage.jsx
+++ Siac/src/Pages/NoPage/nopage.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import Header from '../../Components/Header/header'
+import Rodape from '../../Components/Rodape/rodape'
+import "./nopage.css"
 export default function nopage() {
   return (
-    <div>nopage</div>
+    <>
+      <div className="container">
+        <Header />
+        <div className="message">
+          <h1>PÁGINA NÃO ENCONTRADA</h1>
+          <h2>NOTFOUND 404</h2>
+        </div>
+        <Rodape />
+      </div>
+    </>
   )
 }
